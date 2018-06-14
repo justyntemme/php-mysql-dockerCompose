0b47fcb9beda45c6492e209742b16e80dc5f18a0
Justyn Temme <justyntemme@gmail.com>
2018-06-14 16:08:19 -0500

    added formatting

--- sites/test/index.php
+++ sites/test/index.php
@@ -1,9 +1,12 @@
 <html>
-<meta charset="utf-8">
-<?php
-echo $_SERVER["SERVER_SOFTWARE"];
-echo '<br>';
-echo $_SERVER["HTTP_USER_AGENT"]
-?>
-
+    <meta charset="utf-8">
+    <head>
+    </head>
+    <body>
+        <?php
+            echo $_SERVER["SERVER_SOFTWARE"];
+            echo '<br>';
+            echo $_SERVER["HTTP_USER_AGENT"];
+        ?>
+    </body>
 </html>
